a44e54c2f627f4f15a196082941cdaf862730858
David Asher <david.dm.asher@gmail.com>
2023-10-03 11:00:05 +0300

    chore: adds base url

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -3,6 +3,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
+const rewrites = async () => {
+  return [
+    {
+      source: '/resorces/',
+      destination: 'http://localhost:3000/api/resorces/',
+    },
+    {
+      // test
+      source: '/api/greeting',
+      destination: 'http://localhost:3000/api/greeting',
+    },
+  ];
+};
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -11,6 +25,10 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  rewrites: rewrites,
+  env: {
+    BASE_URL: 'http://localhost:4200',
   },
 };
 
